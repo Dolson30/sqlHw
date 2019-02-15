427a48846bd9324fcbca7182eda2d958a13d7c8a
David Olson <d.olson4295@yahoo.com>
2019-02-15 13:51:03 -0500

    New Commit

--- CustomerView.js
+++ CustomerView.js
@@ -1,7 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var id;
-var num;
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -21,12 +19,70 @@ connection.connect(function(err) {
   start();
 });
 
-function start() {
-    connection.query("SELECT * FROM products", function(err, results) 
-    {
-        if(err) throw err;
+
+function start()
+{
+  inquirer
+    .prompt({
+      name: "choice",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View All Products", "View Low Inventory Products", "Add Items to Inventory","Add a New Product","Exit"]
+    })
+    .then(function(answer) {
+      
+      if (answer.choice === "View All Products") {
+        view();
+      }
+      else if(answer.choice === "View Low Inventory Products") {
+        lowInv();
+      } 
+      else if(answer.choice === "Add Items to Inventory") {
+        addStock();
+      }
+      else if(answer.choice === "Add a New Product")
+      {
+        addProd();
+      }
+      else{
+        connection.end();
+      }
+    });
+}
+
+function view() {
+  connection.query("SELECT * FROM products", function(err, results) 
+  {
+      if(err) throw err;
+          for(let a of results)
+          {
+              console.log("___________________________________________");
+              console.log("Item ID: "+a.item_id);
+              console.log("Product Name: "+a.product_name);
+              console.log("Department: "+a.department_name);
+              console.log("Price: "+a.price);
+              console.log("Quantity: "+a.stock_quantity);
+              console.log("___________________________________________");
+          }
+          start();
+
+  });
+
+}
+
+function lowInv()
+{
+  connection.query("SELECT * FROM products where stock_quantity < 5", function(err, results) 
+  {
+      if(err) throw err;
+          if(results.length === 0)
+          {
+            console.log("All Items Above 5 in Stock!");
+          }
+          else{
             for(let a of results)
             {
+                console.log("___________________________________________");
                 console.log("Item ID: "+a.item_id);
                 console.log("Product Name: "+a.product_name);
                 console.log("Department: "+a.department_name);
@@ -34,71 +90,78 @@ function start() {
                 console.log("Quantity: "+a.stock_quantity);
                 console.log("___________________________________________");
             }
-            prompter();
+        }
+          start();
 
-    });
-
+  });
 }
 
-function prompter()
+
+function addStock()
 {
   inquirer.prompt([
     {
       name: "id",
       type: "prompt",
-      message: "Which item ID would you like to buy?"
+      message: "Which item ID would you like to add inventory to?"
     },{
       name: "num",
       type: "prompt",
-      message: "How Many would you like to buy?"
+      message: "How Many would you like to add?"
     }
   ]).then(function(answer) {
-      id = answer.id;
-      num = answer.num;
-      buyItem();
+   var itemID = parseInt(answer.id);
+    connection.query(
+      "UPDATE products SET stock_quantity = stock_quantity+"+parseInt(answer.num)+" WHERE ?",
+      [
+        {
+          item_id: itemID
+        }
+      ],
+      function(error) {
+        if (error) throw error;
+        console.log("Order placed successfully!");
+        start();
+      }
+    );
   });
 }
 
-function buyItem() {
-  connection.query("SELECT * FROM products", function(err, results) {
-    if (err) throw err;
-    var chosenItem;
-    for (let a of results) 
-        {
-            if (a.item_id = id) 
+  function addProd()
+  {
+    inquirer.prompt([
+      {
+        name: "name",
+        type: "prompt",
+        message: "What is the name of the product?"
+      },{
+        name: "dept",
+        type: "prompt",
+        message: "What is the department name?"
+      },{
+        name: "price",
+        type: "prompt",
+        message: "What is the price per unit?"
+      },{
+        name: "stock",
+        type: "prompt",
+        message: "How many should we stock?"
+      }
+    ]).then(function(answer) {
+        var name = answer.name;
+        var dept = answer.dept;
+        var price = answer.price;
+        var stock = answer.stock;
+        connection.query(
+          "INSERT INTO products SET ?",
             {
-                chosenItem = a;
-            }
-        }
-
-        if (chosenItem.stock_quantity >= num) 
-        {
-          var final = chosenItem.stock_quantity - num;
-          console.log("item count: " + chosenItem.stock_quantity);
-          console.log("User choice: "+num);
-          console.log(final);
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: final
-              },
-              {
-                item_id: id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("Order placed successfully!");
-              console.log("Total Cost: " +chosenItem.price * num);
-              start();
-            }
-          );
-        }
-        else {
-          console.log("You tried to buy too many. Try again...");
-          start();
-        }
-      });
-  };
-
+              product_name:name,department_name:dept,price:price,stock_quantity:stock
+            },
+          function(error) {
+            if (error) throw error;
+            console.log("Item added successfully!");
+            start();
+          }
+        );
+    });
+  }
